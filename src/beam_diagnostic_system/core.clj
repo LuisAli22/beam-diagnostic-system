6e50c85000f749a44d8533e1a02131c0fc027f63
Luis Ali <aliluis@gmail.com>
2015-07-21 22:41:36 -0300

    <Subo el proyecto recién creado>

---
+++ src/beam_diagnostic_system/core.clj
@@ -0,0 +1,7 @@
+(ns beam-diagnostic-system.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/beam_diagnostic_system/core_test.clj
@@ -0,0 +1,7 @@
+(ns beam-diagnostic-system.core-test
+  (:require [clojure.test :refer :all]
+            [beam-diagnostic-system.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
